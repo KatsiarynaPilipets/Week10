98a4d3730100bca0478d434f361cf22eb2854a85
kotenochek27 <katrinya1@gmail.com>
2023-05-13 16:30:18 -0400

    add files

---
+++ script.js
@@ -0,0 +1,28 @@
+function sayLearn() {
+    console.log("Я учу JavaScript");
+}
+sayLearn();
+
+function showPrevious() {
+    console.log("Previous")
+}
+showPrevious();
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function() {
+offset = offset + 550;
+if (offset > 1500) {
+    offset = 0;
+}
+sliderLine.style.left = -offset +'px';
+})
+
+document.querySelector('.slider-prev').addEventListener('click', function() {
+    offset = offset - 550;
+    if (offset < 0) {
+        offset = 1500;
+    }
+    sliderLine.style.left = -offset +'px';
+    })
